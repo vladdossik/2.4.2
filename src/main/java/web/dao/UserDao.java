a5a28abdf566efcf52292f7830eb915b0ee1aa25
vladdosiik <vladdosiik7540@gmail.com>
2021-06-17 13:01:38 +0300

    controllers

--- src/main/java/web/dao/UserDao.java
+++ src/main/java/web/dao/UserDao.java
@@ -3,6 +3,8 @@ package web.dao;
 
 import web.model.User;
 
+import java.util.List;
+
 public interface UserDao {
     User getUserByName(String name);
 }
--- src/main/java/web/model/Role.java
+++ src/main/java/web/model/Role.java
@@ -2,12 +2,18 @@ package web.model;
 
 import org.springframework.security.core.GrantedAuthority;
 
+import javax.persistence.*;
+
 // Этот класс реализует интерфейс GrantedAuthority, в котором необходимо переопределить только один метод getAuthority() (возвращает имя роли).
 // Имя роли должно соответствовать шаблону: «ROLE_ИМЯ», например, ROLE_USER.
+@Entity
+@Table(name="role")
 public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @ManyToMany(mappedBy = "roles")
     private String role;
-
     public Role(Long id, String role) {
         this.id = id;
         this.role = role;
