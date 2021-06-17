17acee92be96fcef835f568995c8b5e76de4b668
vladdosiik <vladdosiik7540@gmail.com>
2021-06-17 13:04:28 +0300

    service

--- src/main/java/web/controller/AdminController.java
+++ src/main/java/web/controller/AdminController.java
@@ -1,4 +1,9 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class AdminController {
+
 }
