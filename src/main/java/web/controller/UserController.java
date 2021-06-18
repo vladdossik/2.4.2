06760a720339cc6f8a00b1a888d5dac09e7cdf89
vladdosiik <vladdosiik7540@gmail.com>
2021-06-18 17:25:37 +0300

    need fix

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,30 +1,30 @@
 
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import web.service.UserService;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Controller
-@RequestMapping("/")
+@RequestMapping(value="/user")
 public class UserController {
 
-    @RequestMapping(value = "hello", method = RequestMethod.GET)
-    public String printWelcome(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Hello!");
-        messages.add("I'm Spring MVC-SECURITY application");
-        messages.add("5.2.0 version by sep'19 ");
-        model.addAttribute("messages", messages);
-        return "hello";
-    }
+    @Autowired
+    UserService userService;
 
-    @RequestMapping(value = "login", method = RequestMethod.GET)
-    public String loginPage() {
-        return "login";
+    @GetMapping()
+    public String showUser(Model model, Authentication authentication) {
+        model.addAttribute("user", userService.getUserByName(authentication.getName()));
+        return "us";
     }
 }
