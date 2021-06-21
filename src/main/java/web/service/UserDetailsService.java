38e4ef19dd75eec804f58cd6cc1733ef226dd364
vladdosiik <vladdosiik7540@gmail.com>
2021-06-21 17:02:06 +0300

    update

---
+++ src/main/java/web/service/UserDetailsService.java
@@ -0,0 +1,6 @@
+package web.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserDetailsService extends org.springframework.security.core.userdetails.UserDetailsService {
+}
